8bfa36dd64370be73048fcf84bc4e9fa507539f3
Aaron Crockett <aaron.crockett.00@gmail.com>
2024-05-29 15:42:05 -0700

    remove stray component and comment content planning

--- apps/web/components/theming/main/index.jsx
+++ apps/web/components/theming/main/index.jsx
@@ -14,9 +14,11 @@ export const Main = () => {
           </Tabs.Trigger>
           <Tabs.Trigger className="" value="tab3">
             UI Mailer
+            {/* TODO: placeholder for content not created. But this is planned to be some sort of UI example. */}
           </Tabs.Trigger>
           <Tabs.Trigger className="" value="tab4">
             UI Dashboard
+            {/* TODO: placeholder for content not created. But this is planned to be some sort of UI example. */}
           </Tabs.Trigger>
         </Tabs.List>
         <Tabs.Content className="" value="tab1"></Tabs.Content>
