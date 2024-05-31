7810dd4a45f13739a50155969063f3a88453ff5c
Aaron Crockett <aaron.crockett.00@gmail.com>
2024-05-31 12:40:50 -0700

    temp layout adjustment for testing

--- apps/web/components/theming/theming.tsx
+++ apps/web/components/theming/theming.tsx
@@ -11,10 +11,42 @@ export const Theming = () => {
   const theming = useTheming();
 
   return (
-    <main>
-      <div>
-        {theming.themeState.colorVars["btn-primary"].DEFAULT}
-
+    <main className="flex">
+      <section className="flex-1">
+        <Tabs.Root defaultValue="tab1">
+          <Tabs.List className="space-x-4 bg-neutral-cool-200 p-4" aria-label="Manage your account">
+            <Tabs.Trigger className="" value="tab1">
+              Theming
+            </Tabs.Trigger>
+            <Tabs.Trigger className="" value="tab2">
+              Code
+            </Tabs.Trigger>
+            <Tabs.Trigger className="" value="tab3">
+              UI Mailer{" "}
+              {/* TODO: placeholder for content not created. But this is planned to be some sort of UI example. */}
+            </Tabs.Trigger>
+            <Tabs.Trigger className="" value="tab4">
+              UI Dashboard{" "}
+              {/* TODO: placeholder for content not created. But this is planned to be some sort of UI example. */}
+            </Tabs.Trigger>
+          </Tabs.List>
+          <Tabs.Content value="tab1">
+            <section className="flex flex-col bg-neutral-cool-300 p-2 ">
+              <Buttons {...theming.buttonProps} />
+              <Inputs {...theming.inputProps} />
+            </section>
+            {/* etc */}
+          </Tabs.Content>
+          <Tabs.Content className="" value="tab2"></Tabs.Content>
+          <Tabs.Content className="" value="tab3">
+            {/* TODO: placeholder for content not created. But this is planned to be some sort of UI example. */}
+          </Tabs.Content>
+          <Tabs.Content className="" value="tab4">
+            {/* TODO: placeholder for content not created. But this is planned to be some sort of UI example. */}
+          </Tabs.Content>
+        </Tabs.Root>
+      </section>
+      <div className="ml-4 flex-1 bg-neutral-cool-50 p-2">
         <Controls>
           <Ctrl title="Buttons">
             <ButtonCtrls updateButtonOpts={theming.updateButtonOpts} {...theming.buttonProps} />
@@ -24,38 +56,6 @@ export const Theming = () => {
           </Ctrl>
         </Controls>
       </div>
-      <Tabs.Root defaultValue="tab1">
-        <Tabs.List className="space-x-4 bg-neutral-cool-200 p-4" aria-label="Manage your account">
-          <Tabs.Trigger className="" value="tab1">
-            Theming
-          </Tabs.Trigger>
-          <Tabs.Trigger className="" value="tab2">
-            Code
-          </Tabs.Trigger>
-          <Tabs.Trigger className="" value="tab3">
-            UI Mailer{" "}
-            {/* TODO: placeholder for content not created. But this is planned to be some sort of UI example. */}
-          </Tabs.Trigger>
-          <Tabs.Trigger className="" value="tab4">
-            UI Dashboard{" "}
-            {/* TODO: placeholder for content not created. But this is planned to be some sort of UI example. */}
-          </Tabs.Trigger>
-        </Tabs.List>
-        <Tabs.Content className=" p-4" value="tab1">
-          <div className="flex flex-col bg-neutral-cool">
-            <Buttons {...theming.buttonProps} />
-            <Inputs {...theming.inputProps} />
-          </div>
-          {/* etc */}
-        </Tabs.Content>
-        <Tabs.Content className="" value="tab2"></Tabs.Content>
-        <Tabs.Content className="" value="tab3">
-          {/* TODO: placeholder for content not created. But this is planned to be some sort of UI example. */}
-        </Tabs.Content>
-        <Tabs.Content className="" value="tab4">
-          {/* TODO: placeholder for content not created. But this is planned to be some sort of UI example. */}
-        </Tabs.Content>
-      </Tabs.Root>
     </main>
   );
 };
