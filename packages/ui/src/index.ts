e971161d9190536db3312ababda6cf8eaba18e1c
Aaron Crockett <aaron.crockett.00@gmail.com>
2024-06-04 13:40:55 -0700

    rough drawer prototype

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,8 +1,9 @@
 // components
 export * from "./components/Button";
+export * from "./components/Checkbox";
+export * from "./components/Drawer";
 export * from "./components/Input";
 export * from "./components/Tabs";
-export * from "./components/Checkbox";
 
 // helpers
 export { createTheme } from "./helpers/create-theme";
